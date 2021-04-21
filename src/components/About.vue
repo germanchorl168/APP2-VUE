269f035a623d2186b4d3b2551dd1241ada346d6b
German Rodriguez <germanrodriguez.estudiante@iser.edu.co>
2021-04-20 19:57:22 -0500

    app2-vue

---
+++ src/components/About.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, eos incidunt. Necessitatibus, dolor. Quod, eos. Libero autem, assumenda dolore atque accusamus, voluptatem et eum, voluptates dignissimos nisi a repellat error?</p>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="jumbotron jumbotron-fluid">
+        
+    <div class="container">
+        <h1 class="display-4">E-Commerce Vue</h1>
+        <p class="lead">Prueba de Vue para cursos de Desarrollo de Webb App con Frameworks</p>
+    </div>
+</div>
+</template>
+
+
